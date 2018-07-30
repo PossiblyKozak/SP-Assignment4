fb5879669142b8fa5a14b203af58e6945b262869
Alex Kozak <a.kozak1@outlook.com>
2018-07-30 00:25:33 -0700

    Cleaned up some of the code, still more to do. Some additional functionality also needs to be completed, we'll see how it goes.

--- chat-client/src/chat-client.c
+++ chat-client/src/chat-client.c
@@ -30,102 +30,90 @@
 #include <time.h>
 #include <signal.h>
 
+// Constants
+#define CURR_SOCKET 9030
 #define INPUT_LINE 17
 #define MAX_MESSAGES 13
 #define MAX_MESSAGE_LENGTH 80
 
 // Prototypes
-bool parseArgument(char *userID, char *serverName, char *argv);
-void *getSendMessage(void *name);
-void *get_in_addr(struct sockaddr *sa);
-void *listener();
+void getIP(char *ip);
+void getTime(char* timeString);
+void getUserInput(char* str);
+void addMessageToDisplay(char* msg);
+int getOutputPos();
+void printMessages();
+void splitMessage(char* str, char* str1, char* str2);
 void sendMessage(int socketID, char* ip, char* name, char* str, char* timeString);
+void *getSendMessage(void *oldName);
+void *listener();
+bool parseArgument(char *userID, char *serverName, char *argv);
 
+// Globals
 char messageQueue[MAX_MESSAGES][MAX_MESSAGE_LENGTH];
 int sockfd;
 int done = 0;
-WINDOW *inputWindow;
-WINDOW *logWindow;
-int line = 2; // Line position of top
-int input = 1; // Line position of top
-int maxx,maxy; // Screen dimensions
+int line = 2; // Line position of messages
 pthread_mutex_t mutexsum = PTHREAD_MUTEX_INITIALIZER;  
 
 
-// current IP: 10.113.16.20
-int main(int argc, char *argv[])
+void getIP(char *ip)
 {
-    initscr();
-	char userID[20], serverName[30];
-	int len;
-    int result;
-    char buf[256];
-    struct sockaddr_in address;
-
-	if ((argc == 3 && (!parseArgument(userID, serverName, argv[1]) || !parseArgument(userID, serverName, argv[2]))) || argc != 3)
-	{
-		printf("Proper Usage: chat-client -user<userID> -server<server name>\n");
-	}
-	else
-	{
-        
-		printw("UserID: %s\t", userID);
-		printw("Server Name: %s\n", serverName);
-
-		// Make socket
-	    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-	    // attr
-	    address.sin_family = AF_INET;
-	    address.sin_addr.s_addr = inet_addr(serverName); //127.0.0.1 for local connections
-        ///////////////////////////////
-	    address.sin_port = htons(9030);
-        ///////////////////////////////
-	    len = sizeof(address);
-
-	    // Make connection
-	    result = connect(sockfd, (struct sockaddr *)&address, len);
-
-	    if(result == -1)
-	    {
-	        perror("Connection failed, try again.\n");
-	    }
-	    else
-	    {
-            write(sockfd,userID,strlen(userID));
-    	    pthread_t threads[2];
-		    void *status;
-		    pthread_attr_t attr;
-		    pthread_attr_init(&attr);
-		    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
-		    // Spawn the listen/receive deamons
-		    pthread_create(&threads[0], &attr, getSendMessage, (void *)userID);
-		    pthread_create(&threads[1], &attr, listener, NULL);
-		    while(!done);
-	    }
-	}	
-    endwin();
-	return 0;
+    //https://stackoverflow.com/questions/1570511/c-code-to-get-the-ip-address
+    int fd;
+    struct ifreq ifr;
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    ifr.ifr_addr.sa_family = AF_INET;
+    snprintf(ifr.ifr_name, IFNAMSIZ, "ens33");
+    ioctl(fd, SIOCGIFADDR, &ifr);
+    /* and more importantly */
+    sprintf(ip, "%s", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+    close(fd);
 }
 
-bool parseArgument(char *userID, char *serverName, char *argv)
+void getTime(char* timeString)
 {
-	bool success = true;
-	if (strstr(argv, "-user") != NULL)
-	{
-		strcpy(userID, argv + 5);
-	}
-	else if (strstr(argv, "-server") != NULL)
-	{
-		strcpy(serverName, argv + 7);
-	}
-	else
-	{
-		success = false;
-	}
+    time_t currTime;
+    struct tm* currTimeInfo;
+    time(&currTime);
+    currTimeInfo = localtime(&currTime);
+    strftime(timeString, 11, "(%H:%M:%S)", currTimeInfo);
+}
 
-	return success;
+void getUserInput(char* str)
+{
+    char newChar = -1;
+    int currIndex = 0;
+    move(INPUT_LINE,0);
+    clrtoeol();
+    move(INPUT_LINE,0);
+    flushinp();
+    while (newChar != 10 || currIndex == 0)
+    {
+        newChar = getch();
+        move(INPUT_LINE + 1, 0);
+        clrtoeol();  
+        if (newChar == 7)
+        {
+            str[--currIndex] = 0;                    
+        }
+        else if (newChar != -1 && currIndex < 80 && newChar > 31 && newChar < 127)
+        {
+            str[currIndex++] = newChar;
+        }
+        move(INPUT_LINE, 0);
+        clrtoeol();  
+        mvprintw(INPUT_LINE, 0, "%s", str, currIndex);  
+
+        move(INPUT_LINE, strlen(str));          
+    }
+}
+
+void addMessageToDisplay(char* msg)
+{
+    memmove(messageQueue[0], messageQueue[1], sizeof(messageQueue) - sizeof(messageQueue[0]));
+    strcpy(messageQueue[MAX_MESSAGES - 1], msg);
+    printMessages();
 }
 
 int getOutputPos()
@@ -149,141 +137,30 @@ void printMessages()
     move(INPUT_LINE, 0);
 }
 
-void addMessageToDisplay(char* msg)
+void splitMessage(char* str, char* str1, char* str2)
 {
-    memmove(messageQueue[0], messageQueue[1], sizeof(messageQueue) - sizeof(messageQueue[0]));
-    strcpy(messageQueue[MAX_MESSAGES - 1], msg);
-    printMessages();
-}
+    int i = 40;
+    bool noSplit = false;
 
-void getIP(char *ip)
-{
-    //https://stackoverflow.com/questions/1570511/c-code-to-get-the-ip-address
-    int fd;
-    struct ifreq ifr;
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
-    ifr.ifr_addr.sa_family = AF_INET;
-    snprintf(ifr.ifr_name, IFNAMSIZ, "ens33");
-    ioctl(fd, SIOCGIFADDR, &ifr);
-    /* and more importantly */
-    sprintf(ip, "%s", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
-    close(fd);
-}
-
-void *getSendMessage(void *oldName)
-{
-    char name[5];
-    strncpy(name, oldName, 5);
-    char str[81];
-    char ip[20];
-    char timeString[11];
-    int bufsize=maxx-4;
-    char *buffer=malloc(bufsize);
-    char str1[41];    
-    char str2[41];    
-    while(!done)
+    while ((str[i] != ' ' && i > 0)) 
     {
-        memset(str, 0, 80);
-        memset(str1, 0, 41);
-        memset(str2, 0, 41);
-        memset(ip, 0, 20);
-        memset(timeString, 0, 20);
-
-        // Get user's message
-        char newChar = -1;
-        int currIndex = 0;
-        move(INPUT_LINE,0);
-        clrtoeol();
-        move(INPUT_LINE,0);
-        flushinp();
-        while (newChar != 10)
+        if (strlen(str) - i >= 40)
         {
-            newChar = getch();
-            if (newChar == 127)
-            {
-                str[--currIndex] = 0;                    
-            }
-            else if (newChar != -1 && currIndex < 80)
-            {
-                str[currIndex++] = newChar;
-            }
-            move(INPUT_LINE, 0);
-            clrtoeol();  
-            mvprintw(INPUT_LINE, 0, "%s", str, currIndex);  
-
-            move(INPUT_LINE, strlen(str));          
+            noSplit = true;
+            i = 40;
+            break;
         }
-        mvprintw(INPUT_LINE + 1, 0, "Sending message...");
-
-        getIP(ip);
-        int c;        
-
-        //printf("%s\n", ip);
-
-        // Time
-        time_t currTime;
-        struct tm* currTimeInfo;
-        char formattedTimeString[28];
-
-        // Get the local time in the tm struct
-        time(&currTime);
-        currTimeInfo = localtime(&currTime);
-
-        // Format the time to the expected style
-        strftime(timeString, 11, "(%H:%M:%S)", currTimeInfo);
-
-        char *current_pos = strchr(str,'\n');
-        while (current_pos)
-        {
-            *current_pos = ' ';
-            current_pos = strchr(current_pos,'\n');
-        }
-
-        if (strlen(str) > 41)
-        {   
-            int i = 40;
-            bool noSplit = false;
-            while ((str[i] != ' ' && i > 0)) 
-            {
-                if (strlen(str) - i >= 40)
-                {
-                    noSplit = true;
-                    i = 40;
-                    break;
-                }
-                i--;
-            }
-            if (noSplit)
-            {
-                strncpy(str1, str, i);
-                strncpy(str2, str + i, strlen(str) - i);
-            }
-            else
-            {
-                strncpy(str1, str, i);
-                strncpy(str2, str + i + 1, strlen(str) - i);
-                str2[strlen(str2) - 1] = 0;
-            }
-
-            // Build the message: "name: message"
-            sendMessage(sockfd, ip, name, str1, timeString);
-            sendMessage(sockfd, ip, name, str2, timeString);      
-        }
-        else
-        {
-            str[strlen(str) - 1] = 0;
-            if(strcmp(str,">>bye<<")==0)
-            {
-                done = 1;
-                memset(str, 0, 80);
-                sprintf(str, ">>bye<<%s", (char*)oldName);
-                write(sockfd,str,strlen(str));
-                pthread_mutex_destroy(&mutexsum);
-                pthread_exit(NULL);
-            }    
-
-            sendMessage(sockfd, ip, name, str, timeString);
-        }
+        i--;
+    }
+    if (noSplit)
+    {
+        strncpy(str1, str, i);
+        strncpy(str2, str + i, strlen(str) - i);
+    }
+    else
+    {
+        strncpy(str1, str, i);
+        strncpy(str2, str + i + 1, strlen(str) - i);
     }
 }
 
@@ -296,14 +173,51 @@ void sendMessage(int socketID, char* ip, char* name, char* str, char* timeString
     addMessageToDisplay(msg);    
 }
 
-void *get_in_addr(struct sockaddr *sa)
+void *getSendMessage(void *oldName)
 {
-    if(sa->sa_family == AF_INET)
+    char str[81], str1[41], str2[41], ip[20], name[5], timeString[11];
+    strncpy(name, oldName, 5);
+    while(!done)
     {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
+        memset(str, 0, 80);
+        memset(ip, 0, 20);
+        memset(timeString, 0, 20);
+        memset(str1, 0, 41);
+        memset(str2, 0, 41);
 
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+        // Get user's message
+
+        getUserInput(str);
+        getIP(ip);
+        getTime(timeString);
+
+        if (strlen(str) > 40)
+        {   
+            splitMessage(str, str1, str2);
+            sendMessage(sockfd, ip, name, str1, timeString);
+            sendMessage(sockfd, ip, name, str2, timeString);  
+        }
+        else
+        {
+            if(strcmp(str,">>bye<<")==0)
+            {
+                done = 1;
+                memset(str, 0, 80);
+                sprintf(str, ">>bye<<%s", (char*)oldName);
+                write(sockfd,str,strlen(str));
+                pthread_mutex_destroy(&mutexsum);
+                pthread_exit(NULL);
+            }    
+            else if(strcmp(str,"/users")==0)
+            {
+                write(sockfd,str,strlen(str));
+            }    
+            else
+            {
+                sendMessage(sockfd, ip, name, str, timeString);                
+            }
+        }
+    }
 }
 
 void *listener()
@@ -314,8 +228,6 @@ void *listener()
     while(!done)
     {
         memset(buffer,0,80);
-        //Receive message from server
-        //Print on own terminal
         if (!(read(sockfd,buffer,bufsize) < 0) && buffer != NULL)
         {
             int n = send(sockfd, buffer, sizeof(buffer), MSG_NOSIGNAL);
@@ -328,11 +240,81 @@ void *listener()
             }
             else
             {
-                if (buffer[0] != 0)
+                if (buffer[0] != 0 && strlen(buffer) > 7)
                 {
                     addMessageToDisplay(buffer);
                 }                
             }            
         }
     }
+}
+
+bool parseArgument(char *userID, char *serverName, char *argv)
+{
+    bool success = true;
+    if (strstr(argv, "-user") != NULL)
+    {
+        strncpy(userID, argv + 5, 20);
+    }
+    else if (strstr(argv, "-server") != NULL)
+    {
+        strcpy(serverName, argv + 7);
+    }
+    else
+    {
+        success = false;
+    }
+
+    return success;
+}
+
+int main(int argc, char *argv[])
+{
+    initscr();
+    keypad(stdscr, TRUE); 
+    char userID[20], serverName[30];
+    int len;
+    int result;    
+
+    if ((argc == 3 && (!parseArgument(userID, serverName, argv[1]) || !parseArgument(userID, serverName, argv[2]))) || argc != 3)
+    {
+        printf("Proper Usage: chat-client -user<userID> -server<server name>\n");
+    }
+    else
+    {
+        
+        printw("UserID: %s\t", userID);
+        printw("Server Name: %s\n", serverName);
+
+        // Make socket
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+        // attr
+        struct sockaddr_in address;
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = inet_addr(serverName); //127.0.0.1 for local connections
+        address.sin_port = htons(CURR_SOCKET);
+        
+        // Make connection
+        if(connect(sockfd, (struct sockaddr *)&address, sizeof(address)) == -1)
+        {
+            perror("Connection failed, try again.\n");
+        }
+        else
+        {
+            write(sockfd,userID,strlen(userID));
+            pthread_t threads[2];
+            void *status;
+            pthread_attr_t attr;
+            pthread_attr_init(&attr);
+            pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+            // Spawn the listen/receive deamons
+            pthread_create(&threads[0], &attr, getSendMessage, (void *)userID);
+            pthread_create(&threads[1], &attr, listener, NULL);
+            while(!done);
+        }
+    }   
+    endwin();
+    return 0;
 }
--- chat-server/src/chat-server.c
+++ chat-server/src/chat-server.c
@@ -12,22 +12,24 @@
 #include <fcntl.h>
 #include <stdbool.h>
 
+// Constants
 #define _GNU_SOURCE
 #define PORT_NUMBER 9030
 
-char *activeUsernames[10];
-pthread_t clientThreads[10];
+// Globals
+char activeUsernames[10][20];
 int activeSockets[10];
-
+pthread_t clientThreads[10];
 int activeSocketCount = 0;
 bool hasAnyoneConnected = false;
 
+// Prototypes
 void error(const char *msg);
-void *getMessages(void *newSocketID);
-void *checkActiveClients();
-void *getNewClients(void *tmpSocketID);
-void sendToAllOtherSockets(int socketID, char* msg);
 void removeClientAtIndex(int i);
+void sendToAllOtherSockets(int socketID, char* msg);
+void *getMessages(void *newSocketID);
+void *getNewClients(void *tmpSocketID);
+void *checkActiveClients();
 
 void error(const char *msg)
 {
@@ -35,14 +37,34 @@ void error(const char *msg)
     exit(1);
 }
 
+void removeClientAtIndex(int i)
+{
+	memmove(&activeSockets[i], &activeSockets[i+1], sizeof(activeSockets) - sizeof(activeSockets[i]));
+	memmove(&activeUsernames[i], &activeUsernames[i+1], sizeof(activeUsernames) - sizeof(activeUsernames[i]));
+	memmove(&clientThreads[i], &clientThreads[i+1], sizeof(clientThreads) - sizeof(clientThreads[i]));			    		
+	activeSocketCount--;
+}
+
+void sendToAllOtherSockets(int socketID, char* msg)
+{
+	sprintf(msg, "%-80s", msg);
+	for (int i = 0; i < activeSocketCount; i++)
+	{
+		if (activeSockets[i] != socketID)
+		{
+			write(activeSockets[i], msg, 80);
+		}
+	}
+}
+
 void *getMessages(void *newSocketID)
 {
-	char messageBuffer[79];
+	char messageBuffer[256];
 	int socket = *(int*)newSocketID;
 	int n;
 	while(1)
 	{
-	    memset(messageBuffer, 0, 79);
+	    memset(messageBuffer, 0, 256);
 	    n = read(socket,messageBuffer,79);
 
 	    if (messageBuffer[0] != 0 && messageBuffer[25] == '>')
@@ -52,6 +74,27 @@ void *getMessages(void *newSocketID)
 	    	messageBuffer[26] = '<';	    		
 	    	n = 0;
 	    	sendToAllOtherSockets(socket, messageBuffer);
+		}
+		else if (messageBuffer[0] == '/')
+		{
+			if (strcmp(messageBuffer, "/users") == 0)
+			{
+				memset(messageBuffer, 0, 256);
+				sprintf(messageBuffer, "%d Users: ", activeSocketCount);
+				for (int i = 0; i < activeSocketCount; i++)
+				{
+					strcat(messageBuffer, activeUsernames[i]);
+					if (activeSockets[i] == socket)
+						strcat(messageBuffer, "(You)");
+					if (i + 1 != activeSocketCount)
+						strcat(messageBuffer, ", ");
+				}
+				write(socket, messageBuffer, sizeof(messageBuffer));
+			}
+			if (strstr(messageBuffer, "/name") != NULL)
+			{
+				
+			}			
 		}
 		else if (messageBuffer[0] != 0)
 		{
@@ -73,12 +116,55 @@ void *getMessages(void *newSocketID)
 	}
 }
 
-void removeClientAtIndex(int i)
+void *getNewClients(void *tmpSocketID)
 {
-	memmove(&activeSockets[i], &activeSockets[i+1], sizeof(activeSockets) - sizeof(activeSockets[i]));
-	memmove(&activeUsernames[i], &activeUsernames[i+1], sizeof(activeUsernames) - sizeof(activeUsernames[i]));
-	memmove(&clientThreads[i], &clientThreads[i+1], sizeof(clientThreads) - sizeof(clientThreads[i]));			    		
-	activeSocketCount--;
+	int socketID = *(int*)tmpSocketID;
+	struct sockaddr_in client_address;
+	socklen_t clilentSize;
+	int newSocketID;
+
+	while (activeSocketCount > 0 || !hasAnyoneConnected )
+    {
+    	while(activeSocketCount == 9){ sleep(1); }
+		listen(socketID,5);
+	    clilentSize = sizeof(client_address);
+	    newSocketID = accept(socketID, (struct sockaddr *)&client_address, &clilentSize);
+	    fcntl(newSocketID, F_SETFL, O_NONBLOCK);
+		
+	    if (newSocketID >= 0)
+	    {
+	    	hasAnyoneConnected = true;
+	    	char message[80];
+	    	char* userName = malloc(40);
+	    	memset(message, 0, 80);	   		
+			while(1)
+			{
+			    if (read(newSocketID,activeUsernames[activeSocketCount],sizeof(activeUsernames[activeSocketCount])) >= 0 && activeUsernames[activeSocketCount] != NULL)
+			    {
+			    	memset(message, 0, 80);				    	
+			    	sprintf(message, "[SERVER] >> User %s entered the chatroom.", activeUsernames[activeSocketCount]);
+			    	printf("%s\n", message);
+			    	sendToAllOtherSockets(socketID, message);
+			        break;  
+			    }
+			}	    	
+	    	pthread_t threads;
+		    pthread_attr_t attr;
+		    pthread_attr_init(&attr);
+		    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+		    clientThreads[activeSocketCount] = threads;
+		    activeSockets[activeSocketCount] = newSocketID;
+		    //strcpy(activeUsernames[activeSocketCount], userName);
+			activeSocketCount++;
+
+			for (int i = 0; i < activeSocketCount; i++)
+		    {
+		    	printf("Socket: %d, userName: %s\n", activeSockets[i], activeUsernames[i]);
+		    }
+			pthread_create(&threads, &attr, getMessages, (void *)&newSocketID);
+	    }
+	}
 }
 
 void *checkActiveClients()
@@ -107,73 +193,6 @@ void *checkActiveClients()
 	        free(buffer);
 		}
 		sleep(1);		
-	}
-}
-
-void *getNewClients(void *tmpSocketID)
-{
-	int socketID = *(int*)tmpSocketID;
-	struct sockaddr_in client_address;
-	socklen_t clilentSize;
-	int newSocketID;
-
-	while (activeSocketCount > 0 || !hasAnyoneConnected )
-    {
-    	while(activeSocketCount == 9){ sleep(1); }
-		listen(socketID,5);
-	    clilentSize = sizeof(client_address);
-	    newSocketID = accept(socketID, (struct sockaddr *)&client_address, &clilentSize);
-	    fcntl(newSocketID, F_SETFL, O_NONBLOCK);
-		
-	    if (newSocketID >= 0)
-	    {
-	    	hasAnyoneConnected = true;
-	    	char message[80];
-	    	char* userName = malloc(20);
-	    	memset(message, 0, 80);	   		
-			while(1)
-			{
-			    memset(userName, 0, 20);
-			    if (read(newSocketID,userName,sizeof(userName)) >= 0 && userName != NULL)
-			    {
-			    	memset(message, 0, 80);				    	
-			    	sprintf(message, "[SERVER] >> User %s entered the chatroom.", userName);
-			    	printf("%s\n", message);
-			    	sendToAllOtherSockets(socketID, message);
-			        break;  
-			    }
-			}	    	
-	    	pthread_t threads;
-		    pthread_attr_t attr;
-		    pthread_attr_init(&attr);
-		    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
-		    clientThreads[activeSocketCount] = threads;
-		    activeSockets[activeSocketCount] = newSocketID;
-		    printf("%s\n", userName);
-		    activeUsernames[activeSocketCount] = userName;
-		    //memcpy(activeUsernames[activeSocketCount], userName, sizeof(userName));
-		    printf("Copy Successful\n");
-			activeSocketCount++;
-			for (int i = 0; i < activeSocketCount; i++)
-		    {
-		    	printf("Socket: %d, userName: %s\n", activeSockets[i], activeUsernames[i]);
-		    }
-			//printf("Starting new thread with ID: %d\nThere are %d active sockets\n", newSocketID, activeSocketCount);
-			pthread_create(&threads, &attr, getMessages, (void *)&newSocketID);
-	    }
-	}
-}
-
-void sendToAllOtherSockets(int socketID, char* msg)
-{
-	sprintf(msg, "%-80s", msg);
-	for (int i = 0; i < activeSocketCount; i++)
-	{
-		if (activeSockets[i] != socketID)
-		{
-			write(activeSockets[i], msg, 80);
-		}
 	}
 }
 
